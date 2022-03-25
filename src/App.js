6bbae989772a9fb70b348a76649bff89169dfa92
evry1lovesjay <62482164+evry1lovesjay@users.noreply.github.com>
2022-03-25 21:06:47 +0100

    Created project with Create-react-app and added files for landing page(homepage).

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
-import React from "react";
-//import logo from './logo.svg';
-import './App.css';
+import "./categories.styles.scss";
 
-import HomePage from './pages/homepage/homepage.component';
-
-function App() {
+const App =()=> {
   return (
-    //default code in app.js deleted by dev.
-    <div>
-      <HomePage />
+    <div className="categories-container">
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body-container">
+          <h2>Jackets</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body-container">
+          <h2>Sneakers</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body-container">
+          <h2>Womens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body-container">
+          <h2>Mens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
     </div>
   );
 }
