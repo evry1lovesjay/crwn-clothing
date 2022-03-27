3956c80dcf13d95a16272a87113c4020761b5ed9
evry1lovesjay <62482164+evry1lovesjay@users.noreply.github.com>
2022-03-27 17:28:47 +0100

    added components folder, routes, sign in components as well as firebase utils and brought in firebase libraries.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,19 @@
-import Directory from "./components/directory/directory.component"
+import {Routes, Route} from "react-router-dom";
 
-const App =()=> {
-  const categories=[
-    {
-      title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-      id: 1,
-    },
-    {
-      title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-      id: 2,
-    },
-    {
-      title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-      id: 3,
-    },
-    {
-      title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-      size: 'large',
-      id: 4,
-    },
-    {
-      title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-      size: 'large',
-      id: 5,
-    },
-  ];
-  
+import Navigation from "./routes/Navigation/navigation.component";
+
+import Home from "./routes/home/home.component"
+import Signin from './routes/sign-in/sign-in.component';
+
+const App = () => {
   return (
-        <Directory categories={categories}/>
+        <Routes>
+          <Route path="/" element={<Navigation />}>
+            <Route index element={<Home />} />
+            <Route path="shop" element={<Home />} />
+            <Route path="sign-in" element={< Signin/>} />
+          </Route>
+        </Routes>
       );
 };
 
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,72 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+import {getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider} from "firebase/auth";
+import {getFirestore, doc, getDoc, setDoc} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAeAhM3r_P2iP0RsV6CYCPKTUAo4XfZzok",
+  authDomain: "crwn-clothing-db-7577e.firebaseapp.com",
+  projectId: "crwn-clothing-db-7577e",
+  storageBucket: "crwn-clothing-db-7577e.appspot.com",
+  messagingSenderId: "345749511675",
+  appId: "1:345749511675:web:aa997044c3e9bc751f7f5e",
+  measurementId: "G-6VNX3WXRFC"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+const provider= new GoogleAuthProvider();
+provider.setCustomParameters({
+    prompt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInwithGooglePopup = ( ) => signInWithPopup(auth, provider);
+
+//Firestore...
+//creating the db equal to calling getFirestore
+export const db = getFirestore();
+
+// to use it.. i.e //adding authenticated users from auth to firestore
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, "users", userAuth.uid);
+
+    // console.log(userDocRef);
+    //snapshot allows us access the data and check if its exists...
+    const userSnapshot = await getDoc(userDocRef);
+    // console.log(userSnapshot)
+    //to check if this document exists, we use the exista method...
+    // console.log(userSnapshot.exists)
+
+    // if user data does not exist.
+    if(!userSnapshot.exists()){
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        // create/ set the document with the data from userAuth in my collection
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            });
+        } catch (error) {
+            console.log("error creating the user", error.message);
+        }
+    }
+    // if user data exists
+    // if true, do nothing, just return the userDocRef
+    return userDocRef;
+    
+
+    
+}
