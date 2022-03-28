6f118fbc837de420d7ac184072d56795fbd62e85
evry1lovesjay <62482164+evry1lovesjay@users.noreply.github.com>
2022-03-28 17:02:51 +0100

    created sign up component, the form input component, and button component.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import {Routes, Route} from "react-router-dom";
 import Navigation from "./routes/Navigation/navigation.component";
 
 import Home from "./routes/home/home.component"
-import Signin from './routes/sign-in/sign-in.component';
+
+import SignIn from './routes/sign-in/sign-in.component';
 
 const App = () => {
   return (
@@ -11,7 +12,7 @@ const App = () => {
           <Route path="/" element={<Navigation />}>
             <Route index element={<Home />} />
             <Route path="shop" element={<Home />} />
-            <Route path="sign-in" element={< Signin/>} />
+            <Route path="sign-in" element={< SignIn/>} />
           </Route>
         </Routes>
       );
